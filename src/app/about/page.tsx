99c97b6b9fc0f99665e82d3b00d7f75452a93927
Piyal Chatterjee <piyal.chatterjee@neudesic.com>
2025-03-10 16:37:07 +0530

    Add About page and update Home page layout

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./about.css";
+const AboutPage = () => {
+  return (
+    <div className="about-heading">
+      <h1>About us!</h1>
+    </div>
+  );
+};
+
+export default AboutPage;
